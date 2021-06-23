31a91f7f3ac49baf53c5ad55e68110d8a7148930
MrWigyea <soad.7ar@gmail.com>
2021-06-23 06:51:42 -0400

    Full login&signup con Spinner populados desde la base de datos(Wamp)

--- app/src/main/java/com/example/pantrya/Login.java
+++ app/src/main/java/com/example/pantrya/Login.java
@@ -63,7 +63,7 @@ public class Login extends AppCompatActivity {
                             data[0] = username;
                             data[1] = password;
 
-                            PutData putData = new PutData("https://pantryaidphp.000webhostapp.com/loginsign/login.php", "POST", field, data);
+                            PutData putData = new PutData("http://192.168.18.7/loginsign/login.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
                                     String result = putData.getResult();
